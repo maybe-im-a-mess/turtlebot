c75f96c3fda6e8f3908e5286c5c8cf2685eeacaa
Olha Solodovnyk <olha.solodovnyk@stud.th-deg.de>
2023-03-09 15:42:29 +0100

    upgraded challenge 1

--- challenges/challenge1.py
+++ challenges/challenge1.py
@@ -6,32 +6,25 @@ class Tb3(Node):
         super().__init__('tb3')
 
         self.cmd_vel_pub = self.create_publisher(
-                Twist,      # message type
-                'cmd_vel',  # topic name
-                1)          # history depth
-        
+                Twist,
+                'cmd_vel',
+                1)
 
         self.scan_sub = self.create_subscription(
                 LaserScan,
                 'scan',
-                self.scan_callback,  # function to run upon message arrival
-                qos_profile_sensor_data)  # allows packet loss
-        
+                self.scan_callback,
+                qos_profile_sensor_data)
 
         self.ang_vel_percent = 0
         self.lin_vel_percent = 0
-        self.x = 0.0
-        self.y = 0.0
-        self.yaw = 0.0
 
         self.state = State.TO_THE_FIRST_WALL
 
     def vel(self, lin_vel_percent, ang_vel_percent=0):
-        """ publishes linear and angular velocities in percent
-        """
-        # for TB3 Waffle
-        MAX_LIN_VEL = 0.26  # m/s
-        MAX_ANG_VEL = 3  # rad/s
+
+        MAX_LIN_VEL = 0.26 
+        MAX_ANG_VEL = 3
 
         cmd_vel_msg = Twist()
         cmd_vel_msg.linear.x = MAX_LIN_VEL * lin_vel_percent / 100
@@ -41,10 +34,7 @@ class Tb3(Node):
         self.ang_vel_percent = ang_vel_percent
         self.lin_vel_percent = lin_vel_percent
 
-
     def scan_callback(self, msg):
-        """ is run whenever a LaserScan msg is received
-        """
         if self.state == State.TO_THE_FIRST_WALL:
             self.to_first_wall(msg)
         elif self.state == State.STOP:
@@ -61,25 +51,6 @@ class Tb3(Node):
         self.vel(lin_vel_percent=0, ang_vel_percent=0)
 
 
-    def stop_wall(self, msg):
-        if msg.ranges[0] < 0.2:
-            self.vel(lin_vel_percent=0, ang_vel_percent=0)
-        else:
-            self.vel(lin_vel_percent=50)
-
-    def turn_wall(self, msg):
-        flage = 'driving'
-
-        if msg.ranges[0] < 0.3:
-            self.vel(lin_vel_percent=0, ang_vel_percent=0)
-            flag = 'stop'
-        if flag == 'stop' and msg.ranges[-90] > 0.4:
-            self.vel(lin_vel_percent=0, ang_vel_percent=85)
-            flag = 'stop'
-        if flag == 'driving':
-            self.vel(lin_vel_percent=50)
-
-
 def main(args=None):
     rclpy.init(args=args)
 
@@ -87,8 +58,7 @@ def main(args=None):
     print('waiting for messages...')
 
     try:
-        rclpy.spin(tb3)  # Execute tb3 node
-        # Blocks until the executor (spin) cannot work
+        rclpy.spin(tb3)
     except KeyboardInterrupt:
         pass
 
