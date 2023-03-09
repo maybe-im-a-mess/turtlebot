b7759e6685c8d84724a7e5329f857eec1e15c8a4
Olha Solodovnyk <olha.solodovnyk@stud.th-deg.de>
2023-03-09 15:44:50 +0100

    Upgraded challenge 2

--- challenges/challenge2.py
+++ challenges/challenge2.py
@@ -1,4 +1,4 @@
-from challenges.tb3 import *
+from tb3 import *
 
 
 class Tb3(Node):
@@ -17,22 +17,14 @@ class Tb3(Node):
                 self.scan_callback,  # function to run upon message arrival
                 qos_profile_sensor_data)  # allows packet loss
         
-
         self.ang_vel_percent = 0
         self.lin_vel_percent = 0
-        self.x = 0.0
-        self.y = 0.0
-        self.yaw = 0.0
 
         self.state = State.TO_THE_FIRST_WALL
 
     def vel(self, lin_vel_percent, ang_vel_percent=0):
-        """ publishes linear and angular velocities in percent
-        """
-        # for TB3 Waffle
-        MAX_LIN_VEL = 0.26  # m/s
-        MAX_ANG_VEL = 3  # rad/s
-
+        MAX_LIN_VEL = 0.26 
+        MAX_ANG_VEL = 3 
         cmd_vel_msg = Twist()
         cmd_vel_msg.linear.x = MAX_LIN_VEL * lin_vel_percent / 100
         cmd_vel_msg.angular.z = MAX_ANG_VEL * ang_vel_percent / 100
@@ -43,8 +35,6 @@ class Tb3(Node):
 
 
     def scan_callback(self, msg):
-        """ is run whenever a LaserScan msg is received
-        """
         if self.state == State.TO_THE_FIRST_WALL:
             self.to_first_wall(msg)
         elif self.state == State.ROTATING:
@@ -76,25 +66,6 @@ class Tb3(Node):
 
     def stop(self):
         self.vel(lin_vel_percent=0, ang_vel_percent=0)
-
-
-    def stop_wall(self, msg):
-        if msg.ranges[0] < 0.2:
-            self.vel(lin_vel_percent=0, ang_vel_percent=0)
-        else:
-            self.vel(lin_vel_percent=50)
-
-    def turn_wall(self, msg):
-        flage = 'driving'
-
-        if msg.ranges[0] < 0.3:
-            self.vel(lin_vel_percent=0, ang_vel_percent=0)
-            flag = 'stop'
-        if flag == 'stop' and msg.ranges[-90] > 0.4:
-            self.vel(lin_vel_percent=0, ang_vel_percent=85)
-            flag = 'stop'
-        if flag == 'driving':
-            self.vel(lin_vel_percent=50)
 
 
 def main(args=None):
